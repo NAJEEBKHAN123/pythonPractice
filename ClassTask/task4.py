046f3ee0b45dd6c4b6e7e71c9a17e8e93475310f
NAJEEBKHAN123 <najeebkhanlaku@gmail.com>
2025-08-23 13:00:32 +0500

    add class code task 4

---
+++ ClassTask/task4.py
@@ -0,0 +1,40 @@
+filename = 'simple.txt'
+
+class TextProcessor:
+    def __init__(self, filename):
+        self.filename = filename
+        self.lines = []
+    
+    def read_file(self):
+        try:
+            with open(self.filename, 'r') as file:
+                self.lines = [line.strip() for line in file.readlines()]
+        except FileNotFoundError:
+            print(f"Error: file {self.filename} not found")
+            self.lines = []
+        
+    def process(self):
+        for line in self.lines:
+            print(line)
+
+class LineNumberPerTextProcessor(TextProcessor):
+    
+    def process(self):
+        for idx, line in enumerate(self.lines, start=1):
+            print(f"{idx} : {line}")
+            
+        
+        
+p = LineNumberPerTextProcessor(filename)
+p.read_file()
+p.process()
+
+        
+        
+        
+    
+    
+
+p = TextProcessor('simple.txt')
+p.read_file()
+p.process()
